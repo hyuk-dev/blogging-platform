c995b6e7994484d7166b295a163575b54bc84e1d
이동혁 <hyuk.development@gmail.com>
2024-07-01 06:31:42 +0000

    jwt error

--- blogging/src/main/java/dev/hyuk/blogging/config/jwt/TokenProvider.java
+++ blogging/src/main/java/dev/hyuk/blogging/config/jwt/TokenProvider.java
@@ -1,13 +1,22 @@
 package dev.hyuk.blogging.config.jwt;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.header.Header;
 import org.springframework.stereotype.Service;
 
 import dev.hyuk.blogging.domain.User;
 import lombok.RequiredArgsConstructor;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +47,37 @@ public class TokenProvider {
           .compact();
 
           // 2. JWT 토큰 유효성 검증 메서드
+        public boolean validToken(String token) {
+            try {
+                Jwts.parser()
+                  .setSigningKey(jwtProperties.getSecretKey()) // 비밀값으로 복호화
+                  .parseClaimsJws(token);
+
+                return true;
+            } catch (Exception e){ // 복호화 과정에서 에러가 나면 유효하지 않은 토큰
+                return false;
+            }
+        }
+
+        // 3) 토큰 기반으로 인증 정보를 가져오는 메서드
+        public Authentication getAuthentication(String token){
+            Claims claims = getClaims(token);
+            Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+            return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities), token, authorities);
+        }
+
+        // 4) 토큰 기반으로 유저 ID를 가져오는 메서드
+        public Long getUserId(String token) {
+            Claims claims = getClaims(token);
+            return claims.get("id", Long.class);
+        }
+
+        public Claims getClaims(String token) {
+            return Jwts.parser() // 클레임 조회
+              .setSigningKey(jwtProperties.getSecretKey())
+              .parseClaimsJws(token)
+              .getBody();
+        }
 
 }
